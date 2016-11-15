3a445fcaddfdb5a0635be09ef39221a3995d3268
Diana Tkachenko <dtkachenko@stitchfix.com>
2016-11-15 12:32:21 -0800

    adding recursive solution to palindrome problem

--- linked_lists/2_6_palindrome.py
+++ linked_lists/2_6_palindrome.py
@@ -4,9 +4,9 @@ from linked_list import Node, LinkedList
 # Implement a function to check if a linked list is a palindrome
 # ==============================================================================================
 def palindrome(head):
-	"""
-	Use stack to insert all elements in list, then pop items out while traversing through list
-	again
+    """
+    Use stack to insert all elements in list, then pop items out while traversing through list
+    again
 	"""
     stack = list()
     node = head
@@ -54,3 +54,33 @@ def palindrome_by_reversal(head):
 print palindrome_by_reversal(LinkedList([1,2,3,2,1]).head) # TRUE
 print palindrome_by_reversal(LinkedList([1,2,7,8,9]).head) # FALSE
 
+def is_palindrome(head):
+    length = get_length(head)
+    node, result = palindrome_rec(head, length)
+    return result
+
+def palindrome_rec(head, length):
+    """
+    Use recursion to find middle node, then pass up the stack next node as recursive calls unwind
+    """
+    if not head or length <= 0:
+        return (head, True)
+    elif length == 1:
+        return (head.next_node, True)
+    
+    returned_node, result = palindrome_rec(head.next_node, length-2)
+    if not result or not returned_node:
+        return (returned_node, result)
+    result = (head.data == returned_node.data)
+    return (returned_node.next_node, result)
+    
+def get_length(node):
+    count = 0
+    while node:
+        count += 1
+        node = node.next_node
+    return count
+
+print is_palindrome(LinkedList([1,2,3,3,2,1]).head) # TRUE
+print is_palindrome(LinkedList([1,2,3,3,3,2,1]).head) # TRUE
+print is_palindrome(LinkedList([1,2,7,8,9]).head) # FALSE

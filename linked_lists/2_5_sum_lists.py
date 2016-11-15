b8831296495a9cf40dcf967acc3c44aa94ce9f34
Diana Tkachenko <dtkachenko@stitchfix.com>
2016-11-14 21:07:50 -0800

    adding sum lists, reverse and forward order

--- linked_lists/2_5_sum_lists.py
+++ linked_lists/2_5_sum_lists.py
@@ -1,3 +1,4 @@
+from linked_list import Node, LinkedList
 # 2.4 Sum Lists
 # ==============================================================================================
 # You have two numbers represented by a linked list, where each node contains a single digit.
@@ -23,6 +24,58 @@ def sum_lists(l1, l2, carry):
 l1 = LinkedList([7,1,6,7,8])
 l2 = LinkedList([5,9,2])
 n = sum_lists(l1.head,l2.head,0)
+while n:
+    print n.data
+    n = n.next_node
+
+# ==============================================================================================
+# FOLLOWUP: Suppose the digits are stored in forward order. Repeat the above problem.
+# ==============================================================================================
+def sum_lists_helper(l1, l2):
+    l1_length = get_length(l1)
+    l2_length = get_length(l2)
+    if l1_length > l2_length:
+        pad_zeroes(l2, l1_length - l2_length)
+    else:
+        pad_zeroes(l1, l2_length - l1_length)
+    node, carry = sum_lists_forward(l1, l2)
+    if carry == 0:
+        return node
+    else:
+        return insert_before(node, carry)
+
+def pad_zeroes(node, count):
+    while node.next_node:
+        node = node.next_node
+    for i in range(count):
+        new_node = Node(0)
+        node.next_node = new_node
+        node = new_node
+        
+def sum_lists_forward(l1, l2):
+    if not l1 and not l2:
+        return (None, 0)
+    node, carry = sum_lists_forward(l1.next_node, l2.next_node)
+    val = carry + l1.data + l2.data
+    full_result = insert_before(node, val % 10)
+    return (full_result, val / 10)
+
+def get_length(node):
+    count = 0
+    while node:
+        count += 1
+        node = node.next_node
+    return count
+    
+def insert_before(head, data):
+    node = Node(data)
+    if head:
+        node.next_node = head
+    return node
+
+l1 = LinkedList([9,7,8])
+l2 = LinkedList([6,8,5])
+n = sum_lists_helper(l1.head,l2.head)
 while n:
     print n.data
     n = n.next_node

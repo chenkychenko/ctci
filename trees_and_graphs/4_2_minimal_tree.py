a18d54f121e486f92702890db9e11a0b8d6dd533
Diana Tkachenko <dtkachenko@stitchfix.com>
2016-11-20 19:45:29 -0800

    creating binary search tree from sorted array

---
+++ trees_and_graphs/4_2_minimal_tree.py
@@ -0,0 +1,42 @@
+# 4.2 Minimal Tree
+# ==============================================================================================
+# Given a sorted (increasing order) array with unique integer elements, write an algorithm to
+# create a binary search tree with minimal height
+# ==============================================================================================
+
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+from collections import deque
+def print_tree(node):
+    queue = deque([])
+    queue.append(node)
+    while queue:
+        cur_node = queue.popleft()
+        print cur_node.data
+        if cur_node.left:
+            queue.append(cur_node.left)
+        if cur_node.right:
+            queue.append(cur_node.right)
+ 
+def minimal_tree(start, end, array):
+    if end == start: # base case
+        return None
+    print "start: {}, end: {}".format(start, end)
+    midpoint = start + (end - start)/2
+    node = Node(array[midpoint])
+    if (end - start) == 1:
+        return Node(array[start])
+    node.left = minimal_tree(start, midpoint, array)
+    node.right = minimal_tree(midpoint+1, end, array)
+    return node
+
+def create_binary_tree(array):
+    return minimal_tree(0, len(array), array)
+
+array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+root = create_binary_tree(array)
+print_tree(root)
